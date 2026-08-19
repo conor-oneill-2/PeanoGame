6f10c13d6ee39df0c4ef6c347a242c44a88942b0
Conor O'Neill <conoroneill71@gmail.com>
2026-08-19 21:21:42 +1000

    is_empty and is_bool methods added to set classes (important for deciders)

--- peano_game/set.py
+++ peano_game/set.py
@@ -4,6 +4,17 @@
 from abc import ABC, abstractmethod
 from typing import final
 
+from peano_game.pawff import Form, Var
+from peano_game.ternary import Ternary
+
+
+class SetExpr:
+    def __init__(self,var:Var,form:Form):
+        self.var=var
+        self.form=form
+
+    def __str__(self)->str:
+        return f"SetExpr({self.var},{self.form})"
 
 class NatSet(ABC):
     @abstractmethod
@@ -12,6 +23,14 @@ class NatSet(ABC):
 
     @abstractmethod
     def simplify(self)->"NatSet":
+        pass
+
+    @abstractmethod
+    def is_empty(self)->Ternary:
+        pass
+
+    @abstractmethod
+    def is_full(self)->Ternary:
         pass
 
 class FiniteNatSet(NatSet):
@@ -25,6 +44,12 @@ class FiniteNatSet(NatSet):
         if not self._items:
             return EmptySet()
         return self
+
+    def is_empty(self)->Ternary:
+        return Ternary.TRUE if not self._items else Ternary.FALSE
+
+    def is_full(self)->Ternary:
+        return Ternary.FALSE
 
     def get_items(self)->set[int]:
         return self._items
@@ -43,6 +68,9 @@ class EmptySet(FiniteNatSet):
     def simplify(self)->NatSet:
         return self
 
+    def is_empty(self)->Ternary:
+        return Ternary.TRUE
+
     def __repr__(self)->str:
         return "EmptySet()"
 
@@ -57,6 +85,12 @@ class FiniteComplementSet(NatSet):
         if not self._items:
             return FullSet()
         return self
+
+    def is_empty(self)->Ternary:
+        return Ternary.FALSE
+
+    def is_full(self)->Ternary:
+        return Ternary.TRUE if not self._items else Ternary.FALSE
 
     def get_items(self)->set[int]:
         return self._items
@@ -104,6 +138,18 @@ class IntersectionSet(NatSet):
 
         return IntersectionSet(*simp_sets)
 
+    def is_empty(self)->Ternary:
+        return Ternary.UNKNOWN
+
+    def is_full(self)->Ternary:
+        res=Ternary.FALSE
+        for set in self._sets:
+            set_res=set.is_full()
+            res|= set_res
+            if res==Ternary.TRUE:
+                break
+        return res
+
     def __repr__(self)->str:
         return f"IntersectionSet({self._sets})"
 
@@ -132,6 +178,18 @@ class UnionSet(NatSet):
             return FiniteComplementSet(all_non_matching_nums)
 
         return UnionSet(*simp_sets)
+
+    def is_empty(self)->Ternary:
+        res=Ternary.TRUE
+        for set in self._sets:
+            set_res=set.is_empty()
+            res&= set_res
+            if res==Ternary.FALSE:
+                break
+        return res
+
+    def is_full(self)->Ternary:
+        return Ternary.UNKNOWN
 
     def __repr__(self)->str:
         return f"UnionSet({self._sets})"
